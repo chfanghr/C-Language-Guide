e7079e7d82ce2e7be187c6a4b07d45ab38cccd26
“chfanghr” <chfanghr@gmail.com>
2018-07-29 13:03:13 +0800

    Update

--- src/Chapter2.c
+++ src/Chapter2.c
@@ -3,7 +3,8 @@
 int main(void)/*主函数*/
 { 
     int num;/*声明num*/
-    printf("Hello World\n");/*调用printf()函数*/
+    printf("Hello World");/*调用printf()函数*/
+    printf("\n";)
     num=100;/*给num赋值100*/
     printf("%d\n",num);/*打印变量num*/
     return 0;/*一切正常,返回0*/
